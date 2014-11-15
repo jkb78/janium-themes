dc5479fe93eaf9f3334547e9a573b85934dbb69a
Juan Carlos Ruiz <juan.carlos.ruiz@janium.com>
2014-11-14 21:00:20 -0600

    Segunda etapa de creación de estructura
    Se hacen correcciones a archivos y se verifica funcionamiento de script
    de shell.

--- build_themes.sh
+++ build_themes.sh
@@ -3,17 +3,16 @@
 set -x
 
 # Compile Janium Stylesheets
-lessc --compress less/bootstrap.less > css/murasaki.min.css
-lessc --compress less/bootstrap-aka.less > css/aka.min.css
-lessc --compress less/bootstrap-ao.less > css/ao.min.css
-lessc --compress less/bootstrap-midori.less > css/midori.min.css
-lessc --compress less/bootstrap-mizuiro.less > css/mizuiro.min.css
-lessc --compress less/bootstrap-momoiro.less > css/momoiro.min.css
-lessc --compress less/bootstrap-orenji.less > css/orenji.min.css
-#lessc --compress less/bootstrap-black-and-blue.less > css/black-and-blue.min.css
-lessc --compress less/bootstrap-modern.less > css/modern.min.css
-lessc --compress less/bootstrap-janium-one.less > css/janium_one.min.css
-lessc --compress less/bootstrap-simply-red.less > css/simply_red.min.css
-lessc --compress less/bootstrap-deep-blue.less > css/deep_blue.min.css
-lessc --compress less/bootstrap-navy.less > css/navy.min.css
-
+lessc --compress less/murasaki > dist/css/murasaki.min.css
+lessc --compress less/aka.less > dist/css/aka.min.css
+lessc --compress less/ao.less > dist/css/ao.min.css
+lessc --compress less/midori.less > dist/css/midori.min.css
+lessc --compress less/mizuiro.less > dist/css/mizuiro.min.css
+lessc --compress less/momoiro.less > dist/css/momoiro.min.css
+lessc --compress less/orenji.less > dist/css/orenji.min.css
+####lessc --compress less/bootstrap-black-and-blue.less > dist/css/black-and-blue.min.css
+#lessc --compress less/modern.less > dist/css/modern.min.css
+#lessc --compress less/janium-one.less > dist/css/janium_one.min.css
+#lessc --compress less/simply-red.less > dist/css/simply_red.min.css
+#lessc --compress less/deep-blue.less > dist/css/deep_blue.min.css
+#lessc --compress less/navy.less > dist/css/navy.min.css
