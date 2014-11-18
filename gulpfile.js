fbc57ff70bd74e7dd8f8a31dc4b6f7029ca92d59
Juan Carlos Ruiz <juan.carlos.ruiz@janium.com>
2014-11-17 23:36:55 -0600

    Se agrega soporte para Gulp
    Primeros pininos con Gulp

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+// gulpfile.js
+
+// --- INIT
+var gulp = require('gulp'),
+    less = require('gulp-less'), // compiles less to CSS
+    //sass = require('gulp-sass'), // compiles sass to CSS
+    minify = require('gulp-minify-css'), // minifies CSS
+    concat = require('gulp-concat'),
+    //uglify = require('gulp-uglify'), // minifies JS
+    rename = require('gulp-rename');
+    //phpunit = require('gulp-phpunit');
+
+// --- TASK
+
+
+// --- WATCH
+
+
+// --- DEFAULT
+
+
